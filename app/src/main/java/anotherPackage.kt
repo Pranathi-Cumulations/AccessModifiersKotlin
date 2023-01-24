6bcbbbcb8526ba1058246186c717a6589103d79e
Pranathi pellakuru <pranathi.p@cumulations.com>
2023-01-24 17:56:36 +0530

    Removed Unused library Module

---
+++ app/src/main/java/anotherPackage.kt
@@ -0,0 +1,7 @@
+package cm.example2.anotherPackage
+
+//can access internal and public variables since in the same module
+fun main(){
+    print("hgjc")
+//    println()
+}
--- app/src/main/java/publicAccessModifier.kt
+++ app/src/main/java/publicAccessModifier.kt
@@ -1,8 +1,10 @@
 package com.example.mylibrary
 
+import com.example.kotlinaccessmodifiers.internalStringExample
 import com.example.kotlinaccessmodifiers.publicStringExample
 
 
 fun main(){
     println(publicStringExample)
+    println(internalStringExample)
 }
