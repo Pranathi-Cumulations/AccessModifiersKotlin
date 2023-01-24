966e3b17c23f6e7575b1cbe1d4d5ca5c056a871a
Pranathi pellakuru <pranathi.p@cumulations.com>
2023-01-24 17:58:59 +0530

    Restructured the code

--- app/src/main/java/anotherPackage.kt
+++
@@ -1,7 +0,0 @@
-package cm.example2.anotherPackage
-
-//can access internal and public variables since in the same module
-fun main(){
-    print("hgjc")
-//    println()
-}
---
+++ app/src/main/java/cm/example2/anotherPackage/anotherPackage.kt
@@ -0,0 +1,10 @@
+package cm.example2.anotherPackage
+
+import com.example.kotlinaccessmodifiers.internalStringExample
+import com.example.kotlinaccessmodifiers.publicStringExample
+
+//can access internal and public variables since in the same module
+fun main(){
+    println(internalStringExample)
+    println(publicStringExample)
+}
